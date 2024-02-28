a6c000fb60383cfe310f99b9595c09852f075c87
Vizax <funkymoostuff@gmail.com>
2024-02-27 19:15:45 -0600

    refactored parse_roll to simplify and output the bits in the return.  also removed the colored library and functions. plain white text it is.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from random import randint
 import re
-from termcolor import colored
 
-
+# this is the main roll function
 def roll_dice(num_dice=1, dice_sides=20):
     roll_list = []
     for _ in range(num_dice):
@@ -10,105 +9,53 @@ def roll_dice(num_dice=1, dice_sides=20):
         roll_list.append(roll)
     return roll_list
 
-
+# this parses the the input
 def parse_roll(roll):
     # Regex for the roll forumula
+    # TODO: validate xdy+z (ex: 1d20+5)
     match = re.match(r"(\d+)d(\d+)(?:\s*([tf])(\d+))?(?:\s+(.*))?", roll)
+    outcome = ""
+    outcome_text = ""
 
-    # number of dicde check
-    if not match.group(1).isdigit() or int(match.group(1)) <= 0:
-        print(
-            colored(
-                "Number must be a positive integer", "red", attrs=["bold", "underline"]
-            )
-        )
-        pass
-    else:
+    try:
         num_dice = int(match.group(1))
-
-    # dice sides check
-    if not match.group(2).isdigit() or int(match.group(2)) <= 0:
-        print(
-            colored(
-                "Number must be a positive integer", "red", attrs=["bold", "underline"]
-            )
-        )
-        pass
-    else:
         num_sides = int(match.group(2))
+        operator = match.group(3) if match.group(3) else ""
+        target = int(match.group(4)) if match.group(4) else ""
+        message = match.group(5) if match.group(5) else ""
 
-    # the actual roll
-    roll_results = roll_dice(num_dice, num_sides)
-    roll_sum = sum(roll_results)
-
-    # "f" or "t" for play style or level style
-    operator = match.group(3) if match.group(3) else ""
-
-    # target number
-    if not match.group(4):
-        target = None
-        pass
-    elif not match.group(4).isdigit() or int(match.group(4)) <= 0:
-        print(
-            colored(
-                "Number must be a positive integer", "red", attrs=["bold", "underline"]
-            )
-        )
-        pass
-    else:
-        target = int(match.group(4))
-
-    # skill or roll message
-    message = match.group(5) if match.group(5) else ""
+        # the actual roll results and sum
+        roll_results = roll_dice(num_dice, num_sides)
+        roll_sum = sum(roll_results)
 
     # outcome statement
-    if not target:
-        outcome = colored(
-            f"ROLL: {num_dice}d{num_sides}: {roll_results} = {roll_sum}", "white",
-        )
-    else:
-        if roll_sum > target:
-            if operator == "f":
-                outcome = colored(
-                    f"FAILURE: {num_dice}d{num_sides}: {roll_results}={roll_sum} which is greater than the {message}({target}",
-                    "red",
-                )
-            if operator == "t":
-                outcome = colored(
-                    f"SUCCESS: {num_dice}d{num_sides}: {roll_results}={roll_sum} which is less than the {message}({target})",
-                    "green",
-                )
-        elif roll_sum < target:
-            if operator == "t":
-                outcome = colored(
-                    f"FAILURE: {num_dice}d{num_sides}: {roll_results}={roll_sum} which is greater than the {message}({target})",
-                    "red",
-                )
-            if operator == "f":
-                outcome = colored(
-                    f"SUCCESS: {num_dice}d{num_sides}: {roll_results}={roll_sum} which is less than the {message}({target})",
-                    "green",
-                )
-        elif roll_sum == target:
-            outcome = colored(
-                f"CRITICAL: {num_dice}d{num_sides}: {roll_results}={roll_sum} which is exactly {message}({target})",
-                "yellow",
-                attrs=["blink", "bold"],
-            )
+        if not target:
+            outcome = "ROLL"
+        else:
+            if (roll_sum > target and operator == "f") or (roll_sum < target and operator == "t"):
+                outcome = "FAILURE"
+            elif (roll_sum < target and operator == "f") or (roll_sum > target and operator == "t"):
+                outcome = "SUCCESS"
+            elif roll_sum == target:
+                outcome = "CRITICAL"
 
-    return outcome
+    except:
+        print(f"ERROR: you typed it in like a dummy, try again.")
+
+
+
+    return outcome, roll, roll_sum, message, target
 
 
 # run the program!
 while True:
-    phrase = input(
-        colored("type roll phrase: (1d20 f10 Sword)", attrs=["bold", "underline"])
-    )
+    phrase = input(f"Type a roll phrase: (1d20 f10 Sword) (6d6): ")
     if phrase == "":
-        print(colored(f"Nothing input, try again.", "red"))
+        print(f"Nothing input, try again.")
         continue
     elif phrase.lower() == "exit" or phrase.lower() == "quit":
-        print(colored(f"Thanks for playing.", "green"))
+        print(f"Thanks for playing.")
         break
     else:
-        print(parse_roll(phrase))
+        overall_results = parse_roll(phrase)
+        print(overall_results)
