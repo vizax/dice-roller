8471f0aa3593ba3615666a0d34ac48cb3b99dfe9
Vizax <funkymoostuff@gmail.com>
2024-03-01 00:13:01 -0600

    added in crit checking regex, crit adding on roll_sum, crit output in output_sentence

--- main.py
+++ main.py
@@ -14,26 +14,32 @@ def roll_dice(num_dice=1, dice_sides=20):
 # this parses the the input text
 def parse_roll(roll):
     # Regex for the roll forumula
-    match = re.match(r"(\d+)d(\d+)(?:\s*([tf])(\d+))?(?:\s+(.*))?", roll)
+    match = re.match(r"(\d+)d(\d+)(?:([\*])(\d+))?(?:\s*([tf])(\d+))?(?:\s+(.*))?", roll)
     outcome = ""
 
     try:
         num_dice = int(match.group(1))
         num_sides = int(match.group(2))
-        operator = match.group(3) if match.group(3) else ""
-        target = int(match.group(4)) if match.group(4) else ""
-        message = match.group(5) if match.group(5) else ""
+        multiplier_used = match.group(3) if match.group(3) else ""
+        multiplier = int(match.group(4)) if match.group(4) else ""
+        operator = match.group(5) if match.group(5) else ""
+        target = int(match.group(6)) if match.group(6) else ""
+        message = match.group(7) if match.group(7) else ""
         over20 = None
 
-        # the actual roll results and sum
+        # the actual roll results          
         roll_results = roll_dice(num_dice, num_sides)
 
-        # check for stats over 20 and adjust the roll_sum
+        # sum results and check for stats over 20 to adjust the roll_sum
         if target != "" and target > 20:
             over20 = target - 20
             roll_sum = sum(roll_results) + over20
         else:
             roll_sum = sum(roll_results)
+        
+        # test for multiplier 
+        if multiplier_used != "":
+            roll_sum = roll_sum * multiplier
 
         # outcome statement
         if not target:
@@ -54,7 +60,7 @@ def parse_roll(roll):
     # rebuild die roll for output purposes
     dice_input = f"{num_dice}d{num_sides}"
 
-    return outcome, dice_input, over20, roll_results, roll_sum, message, target
+    return outcome, dice_input, over20, multiplier_used, multiplier, roll_results, roll_sum, message, target
 
 
 # run the program!
@@ -76,11 +82,16 @@ while True:
         else:
             sentence_bonus = ""
             
-        if overall_results[6] != "":
+        if overall_results[8] != "":
             # used if ther is no target provided
-            sentence_target = f"({overall_results[6]})"
+            sentence_target = f"({overall_results[8]})"
         else:
             sentence_target = ""
+        
+        if overall_results[3] != "":
+            multiplier_bonus = f"*{overall_results[4]}"
+        else:
+            multiplier_bonus = ""
             
-        output_sentence = f"{overall_results[0]}: {overall_results[1]}{sentence_bonus} = {overall_results[3]} = {overall_results[4]} {overall_results[5]}{sentence_target}"
+        output_sentence = f"{overall_results[0]}: {overall_results[1]}{sentence_bonus}{multiplier_bonus} = {overall_results[5]}{multiplier_bonus} = {overall_results[6]} {overall_results[7]}{sentence_target}"
         print(output_sentence)
